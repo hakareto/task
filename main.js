76870ee2c5da5eedf2b06656b23bc30868bdac8d
hakareto <moustafamahmoud1000000@gmail.com>
2024-12-20 15:50:10 +0200

    moustafa

---
+++ main.js
@@ -0,0 +1,35 @@
+var a = 0
+
+// var text = document.getElementById('text')
+// console.log(text);
+
+
+
+
+
+function one(){
+  ++a
+  text.innerText =(a)
+
+  
+}
+
+function two(){
+    if( a > 0){
+        --a
+  text.innerText =(a)
+        
+    }else{
+        
+  text.innerText = `صلي علي النبي `
+        
+    }
+
+
+}
+
+
+
+
+
+
